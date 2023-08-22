5dd9555306bbce1cb13d316c58529c707f2c8111
Jorg Doku <jd728@cornell.edu>
2023-08-22 15:44:02 -0500

    update

--- src/handler.py
+++ src/handler.py
@@ -37,7 +37,7 @@ engine_args = AsyncEngineArgs(
     seed=0,
     worker_use_ray=False,
     max_num_batched_tokens=10000,
-    max_num_seqs=1000
+    max_num_seqs=4096
 )
 
 # Create the vLLM asynchronous engine
