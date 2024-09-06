814f50af3844b9303427d8c1283af065b1060667
pandyamarut <pandyamarut@gmail.com>
2024-09-06 11:03:47 -0700

    fix oai completion api error
    Signed-off-by: pandyamarut <pandyamarut@gmail.com>

--- src/engine.py
+++ src/engine.py
@@ -176,7 +176,8 @@ class OpenAIvLLMEngine(vLLMEngine):
             yield create_error_response(str(e)).model_dump()
             return
         
-        response_generator = await generator_function(request, raw_request=None)
+        dummy_request = DummyRequest()
+        response_generator = await generator_function(request, raw_request=dummy_request)
 
         if not openai_request.openai_input.get("stream") or isinstance(response_generator, ErrorResponse):
             yield response_generator.model_dump()
