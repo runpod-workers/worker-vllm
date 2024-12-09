5f288f8f50b87575754d1673a80aac5025c7e99b
pandyamarut <pandyamarut@gmail.com>
2024-12-09 13:14:03 -0800

    handle exception
    Signed-off-by: pandyamarut <pandyamarut@gmail.com>

--- src/handler.py
+++ src/handler.py
@@ -7,11 +7,15 @@ vllm_engine = vLLMEngine()
 OpenAIvLLMEngine = OpenAIvLLMEngine(vllm_engine)
 
 async def handler(job):
-    job_input = JobInput(job["input"])
-    engine = OpenAIvLLMEngine if job_input.openai_route else vllm_engine
-    results_generator = engine.generate(job_input)
-    async for batch in results_generator:
-        yield batch
+    try:
+        job_input = JobInput(job["input"])
+        engine = OpenAIvLLMEngine if job_input.openai_route else vllm_engine
+        results_generator = engine.generate(job_input)
+        async for batch in results_generator:
+            yield batch
+    except Exception as e:
+        yield {"error": str(e)}
+        return 
 
 runpod.serverless.start(
     {
